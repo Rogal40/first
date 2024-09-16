6b06989815c8ee789f4c3d3a74205ed8eaad7daf
Temirbek Namazbay <t.namazbay@a1qa.com>
2024-09-16 11:48:27 +0500

    First commit

---
+++ src/main/java/com/Lab1/Lab1Namazbay/Lab1NamazbayApplication.java
@@ -0,0 +1,13 @@
+package com.Lab1.Lab1Namazbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1NamazbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1NamazbayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lab1/Lab1Namazbay/Lab1NamazbayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lab1.Lab1Namazbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1NamazbayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
